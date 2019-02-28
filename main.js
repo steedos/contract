a4c50936b98e34db3529c246bddfd917177a8f9e
Jack Zhuang <steedos@users.noreply.github.com>
2019-02-28 11:47:35 +0800

    initial

--- main.js
+++ main.js
@@ -1,74 +1,159 @@
-var fs = require("fs");
-var path = require("path");
-var yaml = require('js-yaml')
+const fs = require("fs");
+const path = require("path");
+const yaml = require('js-yaml');
 
-var appFileName = 'app.json'
-var appObjectFolderName = 'objects'
-var appSrcDirectory = path.join(__dirname, "src");
+const appFileName = 'app.json';
+const objectFolderName = 'objects';
+const triggerFolderName = 'triggers';
+const actionFolderName = 'actions';
+const srcDirectory = path.join(__dirname, "src");
 
-var loadJSONFile = function(filePath){
+let loadObjectsMapping = {}
+
+const loadJSONFile = (filePath)=>{
     return JSON.parse(fs.readFileSync(filePath, 'utf8').normalize('NFC'));
 }
 
-var loadYmlFile = function(filePath){
-    var doc = yaml.safeLoad(fs.readFileSync(filePath, 'utf8'));
-    return doc
+const loadYmlFile = (filePath)=>{
+    return yaml.safeLoad(fs.readFileSync(filePath, 'utf8'));
 }
 
-var loadFile = function(filePath){
+const loadJSFile = (filePath)=>{
+    return require(filePath);
+}
+
+const loadFile = (filePath)=>{
     console.info('loadFile', filePath);
     try {
-        var extname = path.extname(filePath)
+        let extname = path.extname(filePath);
         if(extname.toLocaleLowerCase() == '.json')
-            return loadJSONFile(filePath)
+            return loadJSONFile(filePath);
         else if(extname.toLocaleLowerCase() == '.yml')
-            return loadYmlFile(filePath)
+            return loadYmlFile(filePath);
     } catch (error) {
-        console.error('loadFile error', filePath, error)
+        console.error('loadFile error', filePath, error);
         return {}
     }
 }
 
-var loadApp = function(appFilePath){
-    var app = loadFile(appFilePath)
+const loadApp = (appFilePath)=>{
+    let app = loadFile(appFilePath);
     if(!_.isEmpty(app)){
-        Creator.Apps[app.name] = app
+        Creator.Apps[app.name] = app;
     }
 }
 
-var loadObject = function(objectPath){
-    var appObj =loadFile(objectPath)
-    if(!_.isEmpty(appObj)){
-        Creator.Objects[appObj.name] = appObj
-        Creator.fiberLoadObjects(appObj)
+const loadObjectInCreator = (object)=>{
+    if(!_.isEmpty(object)){
+        Creator.Objects[object.name] = object;
+        Creator.fiberLoadObjects(object);
     }
 }
 
-if(fs.statSync(appSrcDirectory).isDirectory()){
-    fs.readdir(appSrcDirectory, function(err, appFiles){
-        var appFilePath = path.join(appSrcDirectory, appFileName)
-        if(fs.existsSync(appFilePath)){
-            loadApp(appFilePath)
-            fs.watchFile(appFilePath, (curr, prev) => {
-                loadApp(appFilePath)
-            });
-        }
-        var appObjectFolderPath = path.join(appSrcDirectory, appObjectFolderName)
-        if(fs.existsSync(appObjectFolderPath)){
-            fs.readdir(appObjectFolderPath, function(err, appObjectFiles){
-                _.each(appObjectFiles, function(aof){
-                    var objectPath = path.join(appObjectFolderPath, aof)
-                    loadObject(objectPath)
-                    fs.watchFile(objectPath, (curr, prev) => {
-                        loadObject(objectPath)
-                    });
-                })
+const loadObject = (objectPath)=>{
+    let obj =loadFile(objectPath);
+    loadObjectInCreator(obj)
+}
+
+//TODO afterUndelete，beforeSubmit
+const triggerMapping = {
+    beforeInsert: {
+        on: 'server',
+        when: 'before.insert'
+    },
+    beforeUpdate: {
+        on: 'server',
+        when: 'before.update'
+    },
+    beforeDelete: {
+        on: 'server',
+        when: 'before.remove'
+    },
+    afterInsert: {
+        on: 'server',
+        when: 'after.insert'
+    },
+    afterUpdate: {
+        on: 'server',
+        when: 'after.update'
+    },
+    afterDelete: {
+        on: 'server',
+        when: 'after.remove'
+    }
+} 
+const loadTrigger = (triggerPath)=>{
+    let trigger = loadJSFile(triggerPath);
+    let object_name = trigger.object_name
+    if(!object_name){
+        console.error(`load trigger error：Missing attribute 'object_name' /r ${triggerPath}`)
+        return
+    }
+
+    let object = Creator.Objects[object_name]
+    if(!object){
+        console.error(`load trigger error：Invalid 'object_name' /r ${triggerPath}`)
+        return
+    }
+    
+    if(!object.triggers){
+        object.triggers = {}
+    }
+
+    _.each(trigger, (attr, key)=>{
+        console.log('attr', key, attr);
+        let tm = triggerMapping[key]
+        if(!_.isEmpty(tm)){
+            let tkey = `_${key}`.toLocaleUpperCase();
+            object.triggers[tkey] = _.extend({}, tm, {
+                todo: attr
             })
         }
     })
 
+    loadObjectInCreator(object);
+}
+
+if(fs.statSync(srcDirectory).isDirectory()){
+    fs.readdir(srcDirectory, (err, appFiles)=>{
+        //读取 app
+        const appFilePath = path.join(srcDirectory, appFileName)
+        if(fs.existsSync(appFilePath)){
+            loadApp(appFilePath);
+            fs.watchFile(appFilePath, (curr, prev) => {
+                loadApp(appFilePath);
+            });
+        }
+        
+        //读取 object
+        const objectFolderPath = path.join(srcDirectory, objectFolderName)
+        if(fs.existsSync(objectFolderPath)){
+            fs.readdir(objectFolderPath, (err, objectFiles)=>{
+                _.each(objectFiles, (aof)=>{
+                    const objectPath = path.join(objectFolderPath, aof);
+                    loadObject(objectPath);
+                    fs.watchFile(objectPath, (curr, prev) => {
+                        loadObject(objectPath);
+                    });
+                })
+            })
+        }
+
+        //读取 triggers
+        const triggerFolderPath = path.join(srcDirectory, triggerFolderName)
+        if (fs.existsSync(triggerFolderPath)) {
+            fs.readdir(triggerFolderPath, (err, triggerFiles)=>{
+                _.each(triggerFiles, (tf)=>{
+                    const triggerPath = path.join(triggerFolderPath, tf);
+                    loadTrigger(triggerPath)
+                })
+            })
+        }
+
+    })
+
     //监控apps文件夹变化
-    fs.watch(appSrcDirectory, function(eventType, filename){
+    fs.watch(srcDirectory, (eventType, filename)=>{
         console.log('fs.watch', eventType, filename);
     })
 }
