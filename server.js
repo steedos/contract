49fd0b0f609b03068dc41a688889a745bfcd3741
Jack <zhuangjianguo@hotoa.com>
2019-03-02 22:48:04 +0800

    add profile support

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var server = require('@steedos/meteor-bundle-runner');
-var Fiber = require("fibers");
 
-Fiber(function () {
-    server.loadServerBundles();
-    server.callStartupHooks();
-    require("@steedos/steedos-crm-app");
-    require("./steedos-app");
-    server.runMain();
+server.Fiber(function () {
+    server.Profile.run("Server startup", function () {
+        server.loadServerBundles();
+        server.callStartupHooks();
+        require("@steedos/steedos-crm-app");
+        require("./steedos-app");
+        server.runMain();
+    });
 }).run();
