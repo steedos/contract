5f54dafc69c3990dfec4de20e6206c5d798b413d
baozhoutao <baozhoutao@hotoa.com>
2019-05-22 18:17:37 +0800

    使用新版@steedos/objectql

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 var server = require('@steedos/meteor-bundle-runner');
-
+var objectql = require("@steedos/objectql")
+var path = require('path');
 server.Fiber(function () {
     server.Profile.run("Server startup", function () {
         server.loadServerBundles();
+        try {
+            let schema = objectql.getSteedosSchema()
+            schema.addDataSource('contracts', {
+                driver: "mongo",
+                url: "mongodb://127.0.0.1/steedos",
+                objectFiles: [path.resolve(__dirname, "./src")]
+            })
+
+            schema.useAppFile(path.resolve(__dirname, "./src"))
+
+            schema.getDataSource('contracts').createTables()
+
+        } catch (error) {
+            console.log(error)
+        }
         server.callStartupHooks();
-        require("@steedos/steedos-crm-app");
-        require("./index");
         server.runMain();
     });
 }).run();
