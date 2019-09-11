956e9db8a4c844bad14d3e7c05730ab839eb8435
庄建国 <zhuangjianguo@hotoa.com>
2019-09-11 14:32:45 +0800

    Update 1568180513980-organizations-parent.js

--- migrations/1568180513980-organizations-parent.js
+++ migrations/1568180513980-organizations-parent.js
@@ -3,10 +3,8 @@
 let db = require("../db");
 
 // 判断 organizations.parent=“”或不存在，更新为null
-module.exports.up = function (next) {
-  next()
+module.exports.up = async function (next) {
 }
 
-module.exports.down = function (next) {
-  next()
+module.exports.down = async function (next) {
 }
