1a6529146da69722cab8573d807683565f600869
庄建国 <zhuangjianguo@hotoa.com>
2019-09-11 14:21:21 +0800

    Create 1568180513980-organizations-parent.js

---
+++ migrations/1568180513980-organizations-parent.js
@@ -0,0 +1,12 @@
+'use strict'
+
+let db = require("../db");
+
+// 判断 organizations.parent=“”或不存在，更新为null
+module.exports.up = function (next) {
+  next()
+}
+
+module.exports.down = function (next) {
+  next()
+}
