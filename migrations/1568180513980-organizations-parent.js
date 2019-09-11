2ae398cb2ca7140eb49ba1e0fd7e8cc6fccaa5e9
庄建国 <zhuangjianguo@hotoa.com>
2019-09-11 18:38:43 +0800

    migration scripts

--- migrations/1568180513980-organizations-parent.js
+++ migrations/1568180513980-organizations-parent.js
@@ -2,9 +2,28 @@
 
 let db = require("../db");
 
+
 // 判断 organizations.parent=“”或不存在，更新为null
 module.exports.up = async function (next) {
+    let organizations = await db.find("organizations", {
+        filters: [["parent", "=", ""]],
+        fields: ["_id", "parent"]
+    }).catch((ex) => {
+        console.error(ex);
+        return [];
+    });
+    for (let org of organizations) {
+        let updatedDoc = await db.updateOne("organizations", org._id, {parent: null}).catch((ex) => {
+            console.error(ex);
+            return null;
+        });
+        console.log("UPDATE organization.parent: " + updatedDoc.name)
+    }
 }
 
 module.exports.down = async function (next) {
+    let updatedDoc = await db.updateOne("organizations", "51ae9b1a8e296a29c9000002", {parent: ""}).catch((ex) => {
+        console.error(ex);
+        return null;
+    });
 }
--- migrations/1568180513989-company_id.js
+++ migrations/1568180513989-company_id.js
@@ -6,7 +6,7 @@ let db = require("../db");
 let limited_space_ids = [];
 
 let upBySpace = async function (spaceId) {
-    console.log(`migrate up '1568180513989-company_id' START with spaceId: ${spaceId}`);
+    console.log(`SPACEID: ${spaceId}`);
     let companys = await db.find("company", {
         filters: [["space", "=", spaceId]],
         fields: ["_id"]
@@ -15,21 +15,19 @@ let upBySpace = async function (spaceId) {
         return [];
     });
     if (companys.length) {
-        console.log("migrate up '1568180513989-company_id'脚本已跳过：已经有companys记录");
-        console.log(`migrate up '1568180513989-company_id' END with spaceId: ${spaceId}`);
+        console.log("脚本已跳过：已经有companys记录");
         return
     }
 
     // ["is_group", "=", undefined]查的是is_group未定义的情况，对应到mongo语法`is_group: { $exists: false }`
     let orgs = await db.find("organizations", {
-        filters: [["space", "=", spaceId], ["is_company", "=", true], ["is_group", "=", undefined]],
+        filters: [["space", "=", spaceId], [[["is_company", "=", true], ["is_group", "=", undefined]], "or", ["parent", "=", ""]]],
         fields: ["name", "space", "owner", "created_by", "created", "modified_by", "modified"]
     }).catch((ex) => {
         console.error(ex);
         return [];
     });
 
-    console.log(`migrate up '1568180513989-company_id'将插入${orgs.length}条组织对应的company记录`);
     for (let org of orgs) {
         let companyDoc = {
             _id: org._id,
@@ -49,13 +47,12 @@ let upBySpace = async function (spaceId) {
             console.error(ex);
             return null;
         });
-        console.log("migrate up '1568180513989-company_id' inserted company:", insertedDoc._id, insertedDoc.name);
+        console.log("INSERTED company:", insertedDoc.name);
     }
-    console.log(`migrate up '1568180513989-company_id' END with spaceId: ${spaceId}`);
 }
 
 let downBySpace = async function (spaceId) {
-    console.log(`migrate down '1568180513989-company_id' START with spaceId: ${spaceId}`);
+    console.log(`SPACEID: ${spaceId}`);
     let companys = await db.find("company", {
         filters: [["space", "=", spaceId]],
         fields: ["_id"]
@@ -63,22 +60,18 @@ let downBySpace = async function (spaceId) {
         console.error(ex);
         return [];
     });
-    console.log(`migrate down '1568180513989-company_id'将删除${companys.length}条company记录`);
     for (let item of companys) {
         await db.delete("company", item._id).catch((ex) => {
             console.error(ex);
             return [];
         });
-        console.log("migrate down '1568180513989-company_id' deleted company._id:", item._id);
+        console.log("DELETED company:", item.name);
     }
-    console.log(`migrate down '1568180513989-company_id' END with spaceId: ${spaceId}`);
 }
 
 // 判断 company 表为空，执行 company_id 升级
 module.exports.up = async function (next) {
-    console.log("migrate up '1568180513989-company_id' START");
     if (limited_space_ids.length){
-        console.log(`migrate up '1568180513989-company_id' will limited in space: ${limited_space_ids}`);
     }
     else {
         // limited_space_ids为空，则不限制工作区，查出所有工作区ID值循环
@@ -89,19 +82,15 @@ module.exports.up = async function (next) {
             return [];
         });
         limited_space_ids = spaces.map((n)=>{ return n._id; });
-        console.log(`migrate up '1568180513989-company_id' will limited in all space,count: ${limited_space_ids.length}`);
+        console.log(`SPACES count: ${limited_space_ids.length}`);
     }
     for (let spaceId of limited_space_ids) {
         await upBySpace(spaceId);
     }
-    console.log("migrate up '1568180513989-company_id' END");
 }
 
 module.exports.down = async function (next) {
-    console.log("migrate down '1568180513989-company_id' START");
-    console.log(`migrate down '1568180513989-company_id' will limited in space: ${limited_space_ids}`);
     if (limited_space_ids.length) {
-        console.log(`migrate down '1568180513989-company_id' will limited in space: ${limited_space_ids}`);
     }
     else {
         // limited_space_ids为空，则不限制工作区，查出所有工作区ID值循环
@@ -112,10 +101,9 @@ module.exports.down = async function (next) {
             return [];
         });
         limited_space_ids = spaces.map((n) => { return n._id; });
-        console.log(`migrate down '1568180513989-company_id' will limited in all space,count: ${limited_space_ids.length}`);
+        console.log(`SPACES count: ${limited_space_ids.length}`);
     }
     for (let spaceId of limited_space_ids) {
         await downBySpace(spaceId);
     }
-    console.log("migrate down '1568180513989-company_id' END");
 }
