d228f19b33529dc837536adc3273a8cbb1391eb5
庄建国 <zhuangjianguo@hotoa.com>
2019-09-11 18:46:47 +0800

    Update 1568180513980-organizations-parent.js

--- migrations/1568180513980-organizations-parent.js
+++ migrations/1568180513980-organizations-parent.js
@@ -5,25 +5,17 @@ let db = require("../db");
 
 // 判断 organizations.parent=“”或不存在，更新为null
 module.exports.up = async function (next) {
-    let organizations = await db.find("organizations", {
-        filters: [["parent", "=", ""]],
-        fields: ["_id", "parent"]
-    }).catch((ex) => {
-        console.error(ex);
-        return [];
-    });
-    for (let org of organizations) {
-        let updatedDoc = await db.updateOne("organizations", org._id, {parent: null}).catch((ex) => {
-            console.error(ex);
-            return null;
-        });
-        console.log("UPDATE organization.parent: " + updatedDoc.name)
-    }
-}
-
-module.exports.down = async function (next) {
-    let updatedDoc = await db.updateOne("organizations", "51ae9b1a8e296a29c9000002", {parent: ""}).catch((ex) => {
+    let updatedDoc = await db.updateMany("organizations", [["parent", "=", ""]], {parent: null}).catch((ex) => {
         console.error(ex);
         return null;
     });
+    console.log("UPDATE organization.parent: " + updatedDoc)
+}
+
+module.exports.down = async function (next) {
+    let updatedDoc = await db.updateMany("organizations", [["parent", "=", null]], {parent: ""}).catch((ex) => {
+        console.error(ex);
+        return null;
+    });
+    console.log("UPDATE organization.parent: " + updatedDoc)
 }
