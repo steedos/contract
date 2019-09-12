480fe488ccaebce476fe887cdaee32f5a74e5fd7
庄建国 <zhuangjianguo@hotoa.com>
2019-09-12 12:01:36 +0800

    TODO:

--- migrations/1568174204561-init.js
+++ migrations/1568174204561-init.js
@@ -2,7 +2,7 @@
 
 let db = require("../db");
 
-// 初始化数据库，创建初始工作区和初始用户。
+// TODO: 初始化数据库，创建初始工作区和初始用户。
 // 如果初始admin账户已存在，则跳过。
 module.exports.up = async function (next) {
   let users = await db.findOne("users");
--- migrations/1568260766262-username.js
+++ migrations/1568260766262-username.js
@@ -2,7 +2,7 @@
 
 let db = require("../db");
 
-// 如果 space_users.username 不存在，自动更新为对应的 user.username。
+// TODO: 如果 space_users.username 不存在，自动更新为对应的 user.username。
 module.exports.up = async function (next) {
   let space_users = await db.findOne("space_users");
 }
